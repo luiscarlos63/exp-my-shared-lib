1d3f438f134196c1916e076162bbf8bfc16b36c3
Luís Conceição <luiscarlostc92@gmail.com>
2024-05-18 23:25:47 +0100

    save

---
+++ myLibs/valetinho.c
@@ -0,0 +1,11 @@
+
+#include "valetinho.h"
+#include <stdio.h>
+
+int32_t printf_meuDobro(int32_t val)
+{
+
+    printf("\n[VALETINHO] O dobro de %d é igual a %d", val, val*2);
+
+    return val*2;
+}
---
+++ myLibs/valetinho.h
@@ -0,0 +1,9 @@
+#ifndef _VALETINHO_H_
+#define _VALETINHO_H_
+
+#include <stdint.h>
+
+int32_t printf_meuDobro(int32_t val);
+
+
+#endif //!_VALETINHO_H_
